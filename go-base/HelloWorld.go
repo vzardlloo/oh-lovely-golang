9cd8d5517ea8cf9620a3c764e56a7ea411b3c6b4
shihaixia <shihaixia@bonc.com.cn>
2018-04-13 10:22:54 +0800

    :tada: add new code

---
+++ go-base/HelloWorld.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var boy = "hey guy! say hello to the world"
+	fmt.Println(boy)
+
+}
---
+++ go-base/Variables.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var boy, girl string = "I'm a boy", "I'm a girl"
+	fmt.Println(boy, girl)
+
+	boy1, girl1 := "I'm a boy", "I'm a girl";
+	fmt.Println(boy1, girl1)
+
+	_, girl2 := "hahaha", "hehehe"
+	fmt.Println(girl2)
+
+	const Pi = 3.1415926
+	fmt.Println(Pi)
+	var enabled = true
+	fmt.Println(enabled)
+
+}
